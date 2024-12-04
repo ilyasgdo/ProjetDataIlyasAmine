2db3d192fc0df846599f071f7e617b24fcb2a22c
ilyasgdo <ilyas.ghandaoui@edu.esiee.fr>
2024-12-04 15:05:55 +0100

    update .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-1==1
+from data.get_data import get_data
+from dashboard.app import app
+from dashboard.layout import layout
+from dashboard.callbacks import register_callbacks
+
+def main() -> None:
+    data = get_data()  # Supposons que get_data retourne un DataFrame
+    print(data.head())
+
+if __name__ == "__main__":
+    main()
